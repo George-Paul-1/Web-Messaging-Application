0a48513c0b1875c79181f357c5c5721b54f9b44f
George-Paul-1 <george.paul5236@gmail.com>
2024-06-19 13:14:50 +0100

    got basic bones of quitting functionality - improved client disconnects

--- lib/client.py
+++ lib/client.py
@@ -9,17 +9,21 @@ class Client:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.nickname = ""
         self.connected = True 
+        self.history = []
     
+    def get_nickname(self):
+        self.nickname = input("Choose Your Nickname: ")
+        # Once database is built this will query database 
+
+    def get_msg_history(self):
+        pass
+
     def start_(self):
         self.client.connect(ADDR)
         receive_thread = threading.Thread(target=self.receive)
         send_thread = threading.Thread(daemon=True, target=self.send_msg)
         receive_thread.start()
         send_thread.start()
-
-    def get_nickname(self):
-        self.nickname = input("Choose Your Nickname: ")
-        # Once database is built this will query database 
 
     def receive(self):
         while self.connected:
@@ -31,23 +35,42 @@ class Client:
                     print(message)
             except:
                 print("An error occured!")
-                self.client.close()
-                self.connected = False
+                self.disconnect()
                 break
-        self.client.close()
+
+    def calculate_send_length(self, message):
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        return send_length
 
     def send_msg(self):
         while self.connected:
-            message = f"{self.nickname}: {input('')}"
-            if message.endswith(DISCONNECT_MESSAGE):
-                self.connected = False
-            msg_length = len(message)
-            send_length = str(msg_length).encode(FORMAT)
-            send_length += b' ' * (HEADER - len(send_length))
+            message = f"{input('')}"
+            
+            self.history.append(message)
+            print(f"HISTORY: {self.history}")
+
+            send_length = self.calculate_send_length(message)
+
             self.client.send(send_length)
             self.client.send(message.encode(FORMAT))
+            
+            if message.endswith(DISCONNECT_MESSAGE):
+                self.disconnect()
+                break
+
+    def disconnect(self):
+        print("Disconnecting...")
+        try:
+            self.client.close()
+            self.connected = False
+            print("Disconnected")
+        except:
+            print("There was an error disconnecting")
+
+
         
-        self.client.close()
 
 cli = Client()
 cli.get_nickname()
--- lib/server.py
+++ lib/server.py
@@ -8,10 +8,16 @@ server.bind(ADDR)
 
 clients, nicknames = [], []
 
+clients_lock = threading.Lock()
 
 def broadcast(message):
-    for client in clients:
-        client.send(message.encode(FORMAT))
+    with clients_lock:
+        for client in clients:
+            try:
+                client.send(message.encode(FORMAT))
+            except:
+                client.close()
+                clients.remove(client)
 
 
 def handle_client(client, addr):
@@ -22,25 +28,28 @@ def handle_client(client, addr):
             if msg_length:
                 msg_length = int(msg_length)
                 msg = client.recv(msg_length).decode(FORMAT)
+                print(msg)
                 if msg == DISCONNECT_MESSAGE:
-                    client.close()
-                    print("User has left")
-                    break
-                broadcast(msg)
-                print("Message Received By Server")
+                    connected = False
+                else:
+                    broadcast(msg)
+                    print("Message Received By Server")
         except: 
-            client.close()
-            broadcast(f'{addr} has left!')
-            break
+            connected = False
+    
+    with clients_lock:
+        if client in clients:
+            clients.remove(client)
 
     client.close()
     print(f"Connection with {addr} ended") 
+    broadcast(f"{addr} has left the chat.")
 
 
 def start():
     print("[STARTING] server is starting...")
     
-    server.listen(2)
+    server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
     
     while True:
@@ -49,9 +58,9 @@ def start():
 
         client.send('NICK'.encode(FORMAT))
         nickname = client.recv(HEADER).decode(FORMAT)
-        
-        nicknames.append(nickname)
-        clients.append(client)
+        with clients_lock:
+            nicknames.append(nickname)
+            clients.append(client)
 
         print(f"Nickname is: {nickname}")
         broadcast(f"{nickname} joined!\n")
